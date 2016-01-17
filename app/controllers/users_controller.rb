44e493e1b4ccf9b8b06293b4712ba6ab0fc11800
Kunal Sheth <kunal361.sheth@gmail.com>
2016-01-17 23:04:09 +0530

    Added exception handling for admin logic and deleting users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,22 +35,39 @@ class UsersController < ApplicationController
   end
 
   def delete
-    flash[:notice]="User deleted"
-    Users.delete(params[:id])
+    begin
+      if Users.delete(params[:id]) != 0
+        flash[:notice]="User Deleted"
+      else
+        flash[:notice]="No Such User"
+      end
+    rescue
+      flash[:notice]="Error Deleting User"
+    end
     redirect_to users_url
   end
 
   def a_admin
-    user = Users.find(params[:id])
-    user.role = "admin"
-    user.save
+    begin
+      user = Users.find(params[:id])
+      user.role = "admin"
+      user.save
+      flash[:notice]="Admin Added"
+    rescue
+      flash[:notice]="No Such User"
+    end
     redirect_to users_url
   end
 
   def d_admin
-    user = Users.find(params[:id])
-    user.role = nil
-    user.save
+    begin
+      user = Users.find(params[:id])
+      user.role = nil
+      user.save
+      flash[:notice]="Admin Removed"
+    rescue
+      flash[:notice]="No Such User"
+    end
     redirect_to users_url
   end
 
